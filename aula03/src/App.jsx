d2ca1f06e8ce6b54aa1e73eedb0df5faac5f66f8
Gustavo <galbuquerque683@gmail.com>
2025-09-17 10:58:17 -0300

    atualizando projeto MegaSena

---
+++ aula03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
+import EstaComSono from './components/EstaComSono'
+
+const App = () => {
+  return (
+    <div>
+      <EstaComSono comSono ={true} />
+    </div>
+  )
+}
+
+export default App
